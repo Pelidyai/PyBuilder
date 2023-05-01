797a42c8f4de0a6ab30db6ad674baad883bb5e77
Pelidyai <dima-pile@mail.ru>
2023-05-01 21:48:10 +0700

    Add git-tag creation process.

---
+++ src/main/java/com/pickaim/python_builder/action_tree/threads/util/GitTagCreationThread.java
@@ -0,0 +1,23 @@
+package com.pickaim.python_builder.action_tree.threads.util;
+
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.project.Project;
+import com.pickaim.python_builder.action_tree.threads.AbstractBackgroundThread;
+import com.pickaim.python_builder.utils.ProcessRunner;
+import com.pickaim.python_builder.utils.ProjectProperty;
+import org.jetbrains.annotations.Nullable;
+
+public class GitTagCreationThread extends AbstractBackgroundThread {
+    public GitTagCreationThread(@Nullable Project project, String notificationGroupID) {
+        super(project, "Git tag creation", notificationGroupID, "Git tag creation");
+    }
+
+    @Override
+    protected void doThreadAction(ProgressIndicator indicator) throws Exception {
+        String projectPath = ProjectProperty.getInstance(myProject).getProjectPath();
+        String tagName = "v." + ProjectProperty.getInstance(myProject).getCurrentComponent().getVersion();
+        ProcessRunner.runCommand("cmd.exe /c cd /d \"" + projectPath + "\"" +
+                " && " + "git tag -a " + tagName + " -b \"" + tagName + "\"" +
+                " && " + "git push origin " + tagName);
+    }
+}
